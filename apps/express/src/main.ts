ed69cbbf6c7997ec73163d4eafd88f0dbaa5f7bf
Toby Chan <renascence0423@gmail.com>
2023-09-03 22:11:01 +0800

    add current work

--- apps/express/src/main.ts
+++ apps/express/src/main.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 
-const host = process.env.HOST ?? 'localhost';
+const host = process.env.HOST ?? '0.0.0.0';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+  res.send({ message: 'Hello APIs' });
 });
 
 app.listen(port, host, () => {
